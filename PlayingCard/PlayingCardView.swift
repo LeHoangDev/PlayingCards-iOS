2b8dccc853b1b613072f36718bcfd0e2ef48cd92
Manh Hoang Le <hoanglemanh1993@gmail.com>
2019-08-26 11:34:19 +0200

    Draw Circle

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -11,7 +11,24 @@ import UIKit
 class PlayingCardView: UIView {
 
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        /*
+        if let context = UIGraphicsGetCurrentContext(){
+            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0.0, endAngle: 2*CGFloat.pi, clockwise: true)
+            context.setLineWidth(5.0)
+            UIColor.green.setFill()
+            UIColor.black.setStroke()
+            context.strokePath()
+            context.fillPath()
+        }
+         */
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX,y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.black.setStroke()
+        path.stroke()
+        path.fill()
+        
     }
     
 
