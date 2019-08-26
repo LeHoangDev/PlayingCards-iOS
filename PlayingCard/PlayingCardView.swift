a064cf8b68ce6418008dffdb50bc934ec8d5c2a1
Manh Hoang Le <hoanglemanh1993@gmail.com>
2019-08-26 10:47:16 +0200

    PlayingCardView added

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Manh Hoang Le on 20.08.19.
+//  Copyright © 2019 Manh Hoang Le. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
